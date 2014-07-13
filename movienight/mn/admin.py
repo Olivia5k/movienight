00db74fade2c30a9188c7745ae91f13058c63d48
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-07-13 20:00:43 +0200

    Add admin

--- movienight/mn/admin.py
+++ movienight/mn/admin.py
@@ -1,1 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from movienight.mn import models
+
+
+admin.site.register(models.MovieGoer)
+admin.site.register(models.Season)
+admin.site.register(models.WatchlistMovie)
