8ccdc6b78f570254b4a468317d241bb6dec8fb35
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-04-07 22:22:55 +0200

    Complete javascript functions for switching

--- movienight/static/got.js
+++ movienight/static/got.js
@@ -5,6 +5,27 @@ $(document).ready(function(){
         
         setTimeout(function() {
             t.removeClass('inactive').addClass('selected');
-        }, 2500);
+        }, 2600);
+
+        setTimeout(function() {
+            t.find('.person h3').fadeIn(1500);
+            t.find('.avatar').fadeIn(1500, function() {
+                $(this).removeClass('hidden');
+            });
+        }, 6000);
+
+        setTimeout(function() {
+            t.find('.person i').fadeIn(1500);
+        }, 8000);
+
+        setTimeout(function() {
+            var x = 0;
+            t.find('.person ul li').each(function() {
+                var v = $(this);
+                setTimeout(function() {
+                    v.slideDown(1000);
+                }, 2500 * x++);
+            });
+        }, 10000);
     });
 });
